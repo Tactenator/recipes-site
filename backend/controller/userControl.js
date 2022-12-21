125e740fbcc8a268f8716db8c11582e2942c1477
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-12-20 20:55:57 -0600

    adding contexts

--- backend/controller/userControl.js
+++ backend/controller/userControl.js
@@ -1,8 +1,24 @@
 const User = require('../models/users'); 
 const mongoose = require('mongoose'); 
+const jwt = require('jsonwebtoken');
+const { create } = require('../models/users');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const {email, password} = req.body; 
+
+    try {
+        const user = await User.login(email, password); 
+        
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
+    }
+    catch (error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 const signupUser = async (req, res) => {
@@ -11,7 +27,9 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password); 
-        res.status(200).json({ email, user })
+        
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
     }
     catch (error){
         res.status(400).json({error: error.message})
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bycrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
@@ -37,10 +37,31 @@ userSchema.statics.signup = async function (email, password) {
     }
 
     // mypassword
-    const salt = await bycrypt.genSalt(10); 
-    const hash = await bycrypt.hash(password, salt)
+    const hash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10); 
 
     const user = await this.create({email, password: hash })
+
+    return user; 
+}
+
+userSchema.statics.login = async function(email, password){
+    if(!email || !password)
+    {
+        throw Error('All fields are required.')
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect Email'); 
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect password')
+    }
 
     return user; 
 }
