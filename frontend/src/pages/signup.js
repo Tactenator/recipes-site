2d30782a916a0ffd90b7c6de4a1895e565931570
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-12-21 12:23:22 -0600

    adding hooks and setting up signup

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
 
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
+    const [ signup, isLoading, error ] = useSignup
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        await signup(email, password)
         
         console.log(email, password)
     }
