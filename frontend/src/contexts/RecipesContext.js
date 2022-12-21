29a9a0b6b51324edec4da6151e680a6e0dbdd45b
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-12-21 10:20:57 -0600

    Adding hooks

--- frontend/src/contexts/RecipesContext.js
+++ frontend/src/contexts/RecipesContext.js
@@ -5,6 +5,10 @@ export const RecipesContext = createContext()
 
 export const recipesReducer = (state, action) => {
     switch ( action.type ) {
+        case ('CREATE_RECIPE'):
+            return [action.payload, ...state.recipes]
+        case ('SET_RECIPES'):
+            return { recipes: action.payload }
         default: 
             return state
     }
